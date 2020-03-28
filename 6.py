f8b71d348d0d442e9dd019884c30f464254a4eb9
Kairat-Amanzholov <62807685+Kairat-Amanzholov@users.noreply.github.com>
2020-03-28 22:41:14 +0500

    Update 6.py

--- 6.py
+++ 6.py
@@ -5,3 +5,14 @@ t = turtle.Turtle()
 for i in range(5):
     t.forward(150)
     t.right(144)
+   
+   
+   
+   def draw_star(size,color):
+    count = 0
+    angle = 144
+    while count <= 5:
+        turtle.forward(size)
+        turtle.right(angle)
+        count += 1
+    return
